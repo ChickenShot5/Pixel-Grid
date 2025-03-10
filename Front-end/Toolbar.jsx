f5ca1344e3491bc7e0b6532d649bf224dc1b556e
ChickenShot5 <andu_andrei300@yahoo.com>
2025-03-10 15:45:46 +0200

    Pixel Grid
    Full App

---
+++ Front-end/Toolbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Toolbar.css";
+
+const Toolbar = ({selectedColor, setSelectedColor, resetGrid}) => {
+    const colors = ["black", "red", "green", "blue", "yellow", "purple", "orange", "pink"];
+
+    return (
+      <div className="toolbar">
+      {colors.map(color =>(
+        <button key={color}
+        style={{ backgroundColor: color}}
+        onClick={() => setSelectedColor(color)}
+        ></button>
+      ))}
+      </div>
+    );
+};
+
+export default Toolbar;
